15133b93779c7798dda745c82db4e5f38b003b77
Alejandro Droguett <droguettalejandro@gmail.com>
2022-06-16 06:00:42 -0400

    terminado 3 partes

---
+++ assets/js/script3.js
@@ -0,0 +1,15 @@
+document.getElementById("boton1").addEventListener("click", pass)
+
+function pass() {
+    let num1 = document.getElementById("number1").value
+    let num2 = document.getElementById("number2").value
+    let num3 = document.getElementById("number3").value
+
+    if (num1 + num2 + num3 == "911"){
+        msg.innerText = "Password 1 correcto."
+    } else if (num1 + num2 + num3 == "714"){
+        msg.innerText = "Password 2 correcto."
+    } else {
+        msg.innerText = "Password incorrecto."
+    }
+}
